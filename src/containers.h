e2a0fcbd3aecbb218ea7cee64212cdb29f97ef4c
alberto-santini <santini.alberto@gmail.com>
2017-11-10 09:32:23 +0100

    Added check for empty container to join_and_print

--- src/containers.h
+++ src/containers.h
@@ -94,6 +94,7 @@ namespace as {
      */
     template<class Container>
     inline void join_and_print(const Container& container, std::ostream& out = std::cout, std::string separator = ", ") {
+        if(container.begin() == container.end()) { return; }
         for(auto it = container.begin(); it != std::prev(container.end()); ++it) {
             details::iterated_value(*it, out) << separator;
         }
