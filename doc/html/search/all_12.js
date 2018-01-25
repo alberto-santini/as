73569adc9c4edf054ea098fa0ba5e50a4d3914b1
alberto-santini <santini.alberto@gmail.com>
2018-01-25 03:22:04 +0100

    Re-generated the documentation

--- doc/html/search/all_12.js
+++ doc/html/search/all_12.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['vertex_5fcomplement',['vertex_complement',['../namespaceas_1_1graph.html#aec6bce35a79299f552d227442f399580',1,'as::graph']]],
   ['vertices',['vertices',['../namespaceas_1_1graph.html#ab93ee208eb116d3a3349c8de8cc91445',1,'as::graph']]]
 ];
